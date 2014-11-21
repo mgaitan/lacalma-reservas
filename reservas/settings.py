6512b3458bf6610f05c68d97e34947d697826241
Martín Gaitán <gaitan@gmail.com>
2014-11-21 01:06:38 -0300

    allow iframes

--- reservas/settings.py
+++ reservas/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'reservas.urls'
