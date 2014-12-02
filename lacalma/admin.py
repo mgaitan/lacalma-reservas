12a2fe3d5fe32c587dfcc97b92d49528e45f79c6
Martín Gaitán <gaitan@gmail.com>
2014-12-02 17:51:59 -0300

    admin fecha_vencimiento_reserva

--- lacalma/admin.py
+++ lacalma/admin.py
@@ -7,7 +7,7 @@ class DepartamentoAdmin(admin.ModelAdmin):
 
 
 class ReservaAdmin(admin.ModelAdmin):
-    list_display = ('departamento', 'nombre_y_apellido', 'desde', 'hasta', 'estado')
+    list_display = ('departamento', 'nombre_y_apellido', 'desde', 'hasta', 'estado', 'fecha_vencimiento_reserva')
     list_filter = ('departamento', 'estado', 'desde', 'hasta', 'fecha_vencimiento_reserva')
     search_fields = ('nombre_y_apellido', 'email')
 
