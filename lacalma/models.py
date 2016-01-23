fd799d6fa47366580df9e8125ae4c0c4e6566813
Martín Gaitán <gaitan@gmail.com>
2016-01-22 21:44:22 -0300

    fin descuento pago contado

--- lacalma/models.py
+++ lacalma/models.py
@@ -37,7 +37,7 @@ def dias_en_rango(inicio, fin):
 TEMPORADA_ALTA = dias_en_rango(date(2015, 12, 26), date(2016, 2, 15))
 TEMPORADA_MEDIA = dias_en_rango(date(2016, 2, 15), date(2016, 4, 5))  # hasta semana santa
 DESCUENTO_QUINCENA = None     # porciento
-DESCUENTO_PAGO_CONTADO = 5    # porciento
+DESCUENTO_PAGO_CONTADO = None    # porciento
 DEPOSITO_REQUERIDO = 50
 
 
